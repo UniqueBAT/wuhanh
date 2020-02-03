a9ae0ed69a3ecb9d2b5d06efb4e43eb6ce5c2823
wang.zhisheng <wang.zhisheng@jyall.com>
2020-02-03 18:58:45 +0800

    fix:  修改弹窗和按钮样式

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -224,10 +224,7 @@
 					<view class="content">4.7 本平台唯一官方网址：https://onwh.51rry.com（E起支援-疫情物资供需平台)如被人恶意假借名义进行不正当行为，与本平台无关。并且保留对其追究法律责任的权力。</view>
 					<view class="title">第五条 争议解决及法律适用</view>
 					<view class="content">5.1在用户有意向捐赠后，如果在本协议约定内容履行过程中，对相关事宜的履行发生争议，应当协商解决，如协商不成，用户同意按照中华人民共和国颁布的相关法律法规来解决争议，并同意接受湖北省武汉市人民法院的管辖。</view>
-					
-					<navigator @tap="handleModel">
-						<button class="respo-btn">免责申请已阅读完毕</button>
-					</navigator>
+					<view @tap="handleModel" class="respo-btn">免责申请已阅读完毕</view>
 				</view>
 		</view>
 		<mpvue-city-picker themeColor="#007AFF" ref="mpvueCityPicker" :pickerValueDefault="cityPickerValue.pickerValue"
@@ -411,7 +408,6 @@
 				if (this.respoModelCount){
 					this.handleModel()
 				} else {
-					this.respoModelCount = 1
 					this.showRespoModel = true
 				}
 			},
@@ -419,6 +415,7 @@
 				this.showRespoModel = false
 			},
 			handleModel() {
+				this.respoModelCount = 1 // 已阅读免责声明
 				const index = this.selectedIndex
 				if (index == -1) {
 					this.callList = [{
@@ -663,7 +660,7 @@
 		padding: 40upx 40upx;
 		border-radius: 8px;
 		width: 90vw;
-		max-height: 90vh;
+		height: 80vh;
 		overflow-y: auto;
 		&-top {
 			font-size: 12px;
@@ -676,6 +673,12 @@
 			margin-bottom: 30rpx;
 			background: #80ADED;
 			color: #FFFFFF;
+			text-align: center;
+			border-radius: 5px;
+			line-height: 46px;
+			padding-left: 14px;
+			padding-right: 14px;
+			font-size: 16px;
 		}
 		.article {
 			width: 100%;
